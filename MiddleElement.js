b0c1df250e102bc5936524d7ae59cfb5cc0eba4c
Martialis39 <mart_lepanen@hotmail.com>
2017-06-29 08:20:37 +0300

    Update MiddleElement.js

--- MiddleElement.js
+++ MiddleElement.js
@@ -1,14 +1,13 @@
 //Find the Middle Element Kata 
 
-<!-- As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+// As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
 
-The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+//The input to the function will be an array of three distinct numbers (Haskell: a tuple).
 
-For example:
+//For example:
 
-gimme([2, 3, 1]) => 0
+//gimme([2, 3, 1]) => 0
 
--->
 
 var gimme = function (inputArray) {
   
